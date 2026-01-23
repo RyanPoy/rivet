2d6873bed78b5614374d86097b9fdf0beb6a5745
pengyi <ryanpoy@gmail.com>
2026-01-23 11:15:49 +0800

    add tests for Column<T>::ne

--- rivet-orm/src/schema/column_test.rs
+++ rivet-orm/src/schema/column_test.rs
@@ -30,6 +30,7 @@ pub fn test_eq_number() {
         }
     );
 }
+#[test]
 pub fn test_eq_bool() {
     assert_eq!(
         Column::<bool>::new("has_children").eq(true),
@@ -111,18 +112,119 @@ pub fn test_eq_str_ref() {
         }
     );
 }
-// #[test]
-// pub fn test_ne() {
-//     assert_eq!(
-//         Column::<i32>::new("age").ne(20),
-//         Expr::Binary {
-//             left: "age",
-//             op: Op::Ne,
-//             right: Value::I32(Some(20)),
-//         }
-//     );
-// }
-//
+
+#[test]
+pub fn test_ne_number() {
+    assert_eq!(
+        Column::<i32>::new("age").ne(20),
+        Expr::Binary {
+            left: "age",
+            op: Op::Ne,
+            right: Value::I32(Some(20)),
+        }
+    );
+
+    assert_eq!(
+        Column::<Option<i32>>::new("age").ne(20),
+        Expr::Binary {
+            left: "age",
+            op: Op::Ne,
+            right: Value::I32(Some(20)),
+        }
+    );
+
+    assert_eq!(
+        Column::<Option<i32>>::new("age").ne(None),
+        Expr::Binary {
+            left: "age",
+            op: Op::Ne,
+            right: Value::Null,
+        }
+    );
+}
+#[test]
+pub fn test_ne_bool() {
+    assert_eq!(
+        Column::<bool>::new("has_children").ne(true),
+        Expr::Binary {
+            left: "has_children",
+            op: Op::Ne,
+            right: Value::Bool(Some(true)),
+        }
+    );
+    assert_eq!(
+        Column::<Option<bool>>::new("has_children").ne(true),
+        Expr::Binary {
+            left: "has_children",
+            op: Op::Ne,
+            right: Value::Bool(Some(true)),
+        }
+    );
+    assert_eq!(
+        Column::<Option<bool>>::new("has_children").ne(None),
+        Expr::Binary {
+            left: "has_children",
+            op: Op::Ne,
+            right: Value::Null,
+        }
+    );
+}
+#[test]
+pub fn test_ne_string() {
+    assert_eq!(
+        Column::<String>::new("username").ne("Lucy".to_string()),
+        Expr::Binary {
+            left: "username",
+            op: Op::Ne,
+            right: Value::String(Some("Lucy".to_string())),
+        }
+    );
+    assert_eq!(
+        Column::<Option<String>>::new("username").ne("Lucy".to_string()),
+        Expr::Binary {
+            left: "username",
+            op: Op::Ne,
+            right: Value::String(Some("Lucy".to_string())),
+        }
+    );
+    assert_eq!(
+        Column::<Option<String>>::new("username").ne(None::<String>),
+        Expr::Binary {
+            left: "username",
+            op: Op::Ne,
+            right: Value::Null,
+        }
+    );
+}
+#[test]
+pub fn test_ne_str_ref() {
+    assert_eq!(
+        Column::<String>::new("username").ne("Lucy"),
+        Expr::Binary {
+            left: "username",
+            op: Op::Ne,
+            right: Value::String(Some("Lucy".to_string())),
+        }
+    );
+
+    assert_eq!(
+        Column::<Option<String>>::new("username").ne("Lucy"),
+        Expr::Binary {
+            left: "username",
+            op: Op::Ne,
+            right: Value::String(Some("Lucy".to_string())),
+        }
+    );
+    assert_eq!(
+        Column::<Option<String>>::new("username").ne(None::<&str>),
+        Expr::Binary {
+            left: "username",
+            op: Op::Ne,
+            right: Value::Null,
+        }
+    );
+}
+
 // #[test]
 // pub fn test_gt() {
 //     assert_eq!(
