9109adfdba03535a0bada05abde7dcb29fb3bf04
pengyi <ryanpoy@gmail.com>
2026-04-14 14:37:05 +0800

    test: delete meaningless tests

--- rivet-orm/src/sequel/statement/select_tests/test_sqlalchemy.rs
+++ rivet-orm/src/sequel/statement/select_tests/test_sqlalchemy.rs
@@ -503,23 +503,6 @@ fn test_right_nested_inner_join() {
     );
 }
 
-// ----------------------------------------------------------------------------
-// test_filter_by_from_col
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #6414】
-// 测试 filter_by() 在简单 SELECT 上的使用。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// stmt = select(table1.c.myid).filter_by(name="foo")
-// ```
-//
-// 【多 dialect 验证】
-// - MySQL:     WHERE ... = ?
-// - PostgreSQL: WHERE ... = $1
-// - SQLite:    WHERE ... = ?
-// ----------------------------------------------------------------------------
 #[test]
 fn test_filter_by_from_col_equivalent() {
     let stmt = SelectStatement::from(&*MYTABLE)
@@ -543,19 +526,6 @@ fn test_filter_by_from_col_equivalent() {
     );
 }
 
-// ----------------------------------------------------------------------------
-// test_filter_by_from_func
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #6414】
-// 测试 filter_by() 与聚合函数组合使用。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// stmt = select(func.count(table1.c.myid)).filter_by(name="foo")
-// ```
-//
-// ----------------------------------------------------------------------------
 #[test]
 fn test_filter_by_from_func_equivalent() {
     let stmt = SelectStatement::from(&*MYTABLE)
@@ -579,19 +549,6 @@ fn test_filter_by_from_func_equivalent() {
     );
 }
 
-// ----------------------------------------------------------------------------
-// test_filter_by_from_func_not_the_first_arg
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #6414】
-// 测试 filter_by() 与函数组合使用，函数第一个参数不是列的情况。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// stmt = select(func.bar(True, table1.c.myid)).filter_by(name="foo")
-// ```
-//
-// ----------------------------------------------------------------------------
 #[test]
 fn test_filter_by_from_func_not_the_first_arg_equivalent() {
     let stmt = SelectStatement::from(&*MYTABLE)
@@ -639,19 +596,6 @@ fn test_filter_by_from_binary_equivalent() {
     );
 }
 
-// ----------------------------------------------------------------------------
-// test_filter_by_from_label
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #6414】
-// 测试 filter_by() 与带标签的列组合使用。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// stmt = select(table1.c.myid.label("some_id")).filter_by(name="foo")
-// ```
-//
-// ----------------------------------------------------------------------------
 #[test]
 fn test_filter_by_from_label_equivalent() {
     let stmt = SelectStatement::from(&*MYTABLE)
@@ -766,27 +710,6 @@ fn test_methods_generative_group_by() {
     // assert_ne!(format!("{:?}", s1), format!("{:?}", s2));
 }
 
-// ============================================================================
-// ============================================================================
-// ColumnCollectionAsSelectTest 类 - SQLAlchemy ColumnCollectionAsSelectTest 的全部测试
-// ============================================================================
-// ============================================================================
-
-// ----------------------------------------------------------------------------
-// test_c_collection_as_from
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #8285】
-// 测试 select(parent.c) 使用整个列集合作为 SELECT 元素。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// stmt = select(parent.c)
-// eq_(stmt._all_selected_columns, [parent.c.id, parent.c.data])
-// self.assert_compile(stmt, "SELECT parent.id, parent.data FROM parent")
-// ```
-//
-// ----------------------------------------------------------------------------
 #[test]
 fn test_c_collection_as_from() {
     let stmt = SelectStatement::from(&*PARENT)
@@ -808,229 +731,4 @@ fn test_c_collection_as_from() {
         r#"SELECT "parent0"."id", "parent0"."data" FROM "parent" AS "parent0""#,
         []
     );
-}
-
-// ----------------------------------------------------------------------------
-// test_c_sub_collection_str_stmt
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #8285】
-// 测试通过字符串索引选择列子集合。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// stmt = select(table1.c["myid", "description"])
-// ```
-//
-// ----------------------------------------------------------------------------
-#[test]
-fn test_c_sub_collection_str_stmt() {
-    let stmt = SelectStatement::from(&*MYTABLE)
-        .select(MYTABLE.column("myid"))
-        .select(MYTABLE.column("description"));
-
-    assert_mysql!(
-        &stmt,
-        "SELECT `mytable0`.`myid`, `mytable0`.`description` FROM `mytable` AS `mytable0`",
-        []
-    );
-    assert_pg!(
-        &stmt,
-        r#"SELECT "mytable0"."myid", "mytable0"."description" FROM "mytable" AS "mytable0""#,
-        []
-    );
-    assert_sqlite!(
-        &stmt,
-        r#"SELECT "mytable0"."myid", "mytable0"."description" FROM "mytable" AS "mytable0""#,
-        []
-    );
-}
-
-// ----------------------------------------------------------------------------
-// test_c_sub_collection_int_stmt
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #8285】
-// 测试通过整数索引选择列子集合，并重新排序。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// stmt = select(table1.c[2, 0])  # 先选 index 2，再选 index 0
-// ```
-//
-// 【翻译说明】
-// table1 的列顺序是 [myid (0), name (1), description (2)]
-// c[2, 0] 意味着先选 description，再选 myid
-//
-// ----------------------------------------------------------------------------
-#[test]
-fn test_c_sub_collection_int_stmt() {
-    // table1 列顺序: myid (0), name (1), description (2)
-    // c[2, 0] 意味着先选 index 2 (description)，再选 index 0 (myid)
-    let stmt = SelectStatement::from(&*MYTABLE)
-        .select(MYTABLE.column("description"))
-        .select(MYTABLE.column("myid"));
-
-    assert_mysql!(
-        &stmt,
-        "SELECT `mytable0`.`description`, `mytable0`.`myid` FROM `mytable` AS `mytable0`",
-        []
-    );
-    assert_pg!(
-        &stmt,
-        r#"SELECT "mytable0"."description", "mytable0"."myid" FROM "mytable" AS "mytable0""#,
-        []
-    );
-    assert_sqlite!(
-        &stmt,
-        r#"SELECT "mytable0"."description", "mytable0"."myid" FROM "mytable" AS "mytable0""#,
-        []
-    );
-}
-
-// ----------------------------------------------------------------------------
-// test_c_sub_collection_str
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #8285】
-// 测试列子集合的字符串索引访问。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// coll = table1.c["myid", "description"]
-// is_(coll.myid, table1.c.myid)  # 验证可以按名称访问
-// eq_(list(coll), [table1.c.myid, table1.c.description])
-// ```
-//
-// ----------------------------------------------------------------------------
-#[test]
-fn test_c_sub_collection_str() {
-    // SQLAlchemy: coll = table1.c["myid", "description"]
-    // rivet-orm: 通过 Table.column("name") 访问
-    let col_myid = MYTABLE.column("myid");
-    let col_desc = MYTABLE.column("description");
-
-    // 验证可以访问到正确的列
-    assert_eq!(col_myid.name, "myid");
-    assert_eq!(col_desc.name, "description");
-}
-
-// ----------------------------------------------------------------------------
-// test_c_sub_collection_int
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #8285】
-// 测试列子集合的整数索引访问。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// coll = table1.c[2, 0]
-// is_(coll.myid, table1.c.myid)
-// eq_(list(coll), [table1.c.description, table1.c.myid])
-// ```
-//
-// ----------------------------------------------------------------------------
-#[test]
-fn test_c_sub_collection_int() {
-    // SQLAlchemy: coll = table1.c[2, 0]
-    // rivet-orm: 通过 Table.column("name") 访问
-    let col_myid = MYTABLE.column("myid");
-    let col_desc = MYTABLE.column("description");
-
-    assert_eq!(col_myid.name, "myid");
-    assert_eq!(col_desc.name, "description");
-}
-
-// ----------------------------------------------------------------------------
-// test_c_sub_collection_positive_slice
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #8285】
-// 测试正数切片访问。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// coll = table1.c[0:2]  # 切片 [0, 1)，即 index 0 和 1
-// ```
-//
-// ----------------------------------------------------------------------------
-#[test]
-fn test_c_sub_collection_positive_slice() {
-    // SQLAlchemy: c[0:2] 选择 index 0 和 1 (不含 2)
-    // rivet-orm: 通过 Table.column("name") 访问
-    let col_myid = MYTABLE.column("myid");
-    let col_name = MYTABLE.column("name");
-
-    assert_eq!(col_myid.name, "myid");
-    assert_eq!(col_name.name, "name");
-}
-
-// ----------------------------------------------------------------------------
-// test_c_sub_collection_negative_slice
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #8285】
-// 测试负数切片访问。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// coll = table1.c[-2:]  # 最后两个元素，即 index 1 和 2
-// ```
-//
-// ----------------------------------------------------------------------------
-#[test]
-fn test_c_sub_collection_negative_slice() {
-    // SQLAlchemy: c[-2:] 选择最后两个元素
-    // table1 列: [myid (0), name (1), description (2)]
-    // c[-2:] 选择 index 1 和 2，即 name 和 description
-    let col_name = MYTABLE.column("name");
-    let col_desc = MYTABLE.column("description");
-
-    assert_eq!(col_name.name, "name");
-    assert_eq!(col_desc.name, "description");
-}
-
-// ----------------------------------------------------------------------------
-// test_missing_key
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #8285】
-// 测试访问不存在的键时是否抛出 KeyError。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// with expect_raises_message(KeyError, "unknown"):
-//     table1.c["myid", "unknown"]
-// ```
-//
-// 【状态】#[ignore] - 错误处理机制不同
-// ----------------------------------------------------------------------------
-#[test]
-#[ignore = "rivet-orm 的列访问错误处理机制与 SQLAlchemy 不同"]
-fn test_missing_key() {
-    // SQLAlchemy: table1.c["myid", "unknown"] 抛出 KeyError("unknown")
-    // rivet-orm: Table.column("unknown") 在编译时或运行时失败
-}
-
-// ----------------------------------------------------------------------------
-// test_missing_index
-// ----------------------------------------------------------------------------
-//
-// 【SQLAlchemy 原版测试说明 - test issue #8285】
-// 测试使用越界整数索引时是否抛出 IndexError。
-//
-// 【SQLAlchemy 原版代码】
-// ```python
-// with expect_raises_message(IndexError, "5"):
-//     table1.c["myid", 5]
-// ```
-//
-// 【状态】#[ignore] - rivet-orm 不支持整数索引访问
-// ----------------------------------------------------------------------------
-#[test]
-#[ignore = "rivet-orm 不支持整数索引访问列"]
-fn test_missing_index() {
-    // SQLAlchemy: table1.c["myid", 5] 中 "myid" 存在，但 5 越界
-    // 抛出 IndexError("5")
-    // rivet-orm 不支持这种方式
 }
